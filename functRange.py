bb9a9c8600eaf9e57ac4855324fbabd14458946f
koueAnicet <68559596+koueAnicet@users.noreply.github.com>
2022-02-17 16:41:17 +0000

    exercice de demain: functions natives python

---
+++ functRange.py
@@ -0,0 +1,30 @@
+# def getRange():
+#     i =int(input('nombre1 min:\t'))
+#     j=int(input('nombre2 max:\t'))
+#     c=[]
+#     while i < j:
+#         c.append(i)
+#         i+=1
+#     print(c)
+# getRange()
+#---------------  
+# def autre(n,m):
+#     n=n-1
+#     m=m-1
+#     boite=[]
+#     while n< m:
+#         n+=1
+#         boite.append(n)
+#     print(boite)
+
+# autre(3,10)
+
+def autreFor(n,m):
+    n-=1
+    m-=1
+    boite=[]
+    for i in  m:
+        boite.append(i)
+    print(boite)
+
+autreFor(3,10)
